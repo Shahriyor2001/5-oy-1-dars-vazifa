dcf6eb8fe51c074d989730cf659222fab42125c4
shahriyor2001 <101494368+Shahriyor2001@users.noreply.github.com>
2024-11-05 14:27:02 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("5-oy 1-dars vazifa")
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Najot Ta'lim");
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log("Server is running on port 3000");
